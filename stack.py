941e556a74d00a709d9632af1714393161323f75
devidy <devidy.oliveira@gmail.com>
2020-01-19 20:33:31 -0300

    comentarios

--- stack.py
+++ stack.py
@@ -2,24 +2,27 @@ class Stack:
 	
 	def __init__(self):
 		self.stack = []
-
+    #insere na pilha
 	def push(self, e):
 		self.stack.append(e)
 
+    #remove ultimo elemento
 	def pop(self):
 		if not self.empty():
 			self.stack.pop(len(self.stack) - 1)
 
+    #retorna ultimo elemento
 	def top(self):
 		if not self.empty():
 			return self.stack[-1]
 		return None
 
+    #verifica se a pilha esta vazia
 	def empty(self):
 		if (len(self.stack) == 0):
 			return True
 		return False
-
+    #
 	def length(self):
 		return len(self.stack)
 
